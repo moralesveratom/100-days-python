a659a925fe01275dd80d703680f0902c83f13de8
Tomás Morales Vera <moralesveratomas@gmail.com>
2023-01-15 10:30:49 -0300

    cambios
    Subo cambios de dia 3

---
+++ day-03/day-03.py
@@ -0,0 +1,57 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+option_one = input('Left or right?')
+
+if option_one == 'left':
+    option_two = input('swim or wait?')
+
+    if option_two == 'wait':
+        option_three = input('Which door?')
+
+        if option_three == 'yellow':
+            print('You Win!')
+
+        elif option_three == 'red':
+            print('Bumed byfire.')
+            print('Game 0ver.')
+
+        elif option_three == 'blue':
+            print('Eaten bybeasts.')
+            print('Game Over.')
+
+        else:
+            print('Game Over.')
+
+    else:
+        print('Attacked by trout.')
+        print('Game Over.')
+
+else:
+    print('Fall intoa hole.')
+    print('Game Over.')
